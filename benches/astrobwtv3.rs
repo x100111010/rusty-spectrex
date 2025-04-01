1dc8de58673ec2cb3b4f1aed4e1d32d5ea2051f6
x100111010 <167847953+x100111010@users.noreply.github.com>
2025-04-02 01:39:34 +0200

    more verbose tests and more benchmarks
    - also fixed the sample_size warning

--- benches/astrobwtv3.rs
+++ benches/astrobwtv3.rs
@@ -1,5 +1,6 @@
 // Public crates.
 use criterion::{criterion_group, criterion_main, Criterion};
+use rand::random;
 
 // Private crates.
 use spectrex::astrobwtv3;
@@ -9,9 +10,23 @@ fn astrobwtv3_bench(input: &[u8; 32]) {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let input: [u8; 32] = [0; 32];
-    c.bench_function("astrobwtv3", |b| b.iter(|| astrobwtv3_bench(&input)));
+    c.bench_function("astrobwtv3_zeros", |b| {
+        b.iter(|| astrobwtv3_bench(&[0; 32]))
+    });
+
+    c.bench_function("astrobwtv3_ones", |b| {
+        b.iter(|| astrobwtv3_bench(&[0xFF; 32]))
+    });
+
+    c.bench_function("astrobwtv3_random", |b| {
+        let random_data = random::<[u8; 32]>();
+        b.iter(|| astrobwtv3_bench(&random_data))
+    });
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(50);
+    targets = criterion_benchmark
+}
 criterion_main!(benches);
